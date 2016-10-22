378699f367d946a88058e7ebb8ace28fd3faa95b
jeffrey <jeffrey@steamboat>
2016-10-22 16:32:25 +0200

    [MOD] Site Title Suffix

---
+++ components/SiteTitle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Startup operations, keep fast!
+ * @link http://www.yiiframework.com/doc-2.0/guide-runtime-bootstrapping.html
+ */
+
+namespace app\components;
+
+class SiteTitle extends \yii\base\Component
+{
+
+    public function init()
+    {
+        \Yii::$app->view->on(\yii\web\View::EVENT_AFTER_RENDER, function ($event) {
+            if (\yii::$app->view->title != \Yii::$app->name) {
+                \yii::$app->view->title = " - " . \Yii::$app->name;
+            }
+        });
+    }
+
+}
--- config/params.php
+++ config/params.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    
     'adminEmail' => 'jeffrey@humanized.be',
     //Single Event Setup
     'dateStart' => '20161101', 'dateStop' => '20161105', 'dateExceptions' => [],
