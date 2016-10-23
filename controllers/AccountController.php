def51be23cf9a308231bd173eb29786bd7ce64be
jeffrey <jeffrey@steamboat>
2016-10-23 18:13:03 +0200

    [MOD] Signing up without mail

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -5,7 +5,7 @@ namespace app\controllers;
 use Yii;
 use humanized\user\controllers\DefaultController as ParentController;
 use humanized\user\models\LoginForm;
-use humanized\user\models\SignupForm;
+use app\models\SignupForm;
 
 /**
  * SupplyController implements the CRUD actions for Supply model.
@@ -16,13 +16,21 @@ class AccountController extends ParentController
     public function actionIndex()
     {
         if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
+            return $this->goBack();
         }
 
         $login = new LoginForm();
-        $signup = new SignupForm();
+        $signup = new \app\models\SignupForm;
         if ($login->load(Yii::$app->request->post()) && $login->login()) {
             return $this->goBack();
+        }
+
+        if ($signup->load(Yii::$app->request->post())) {
+
+            if (NULL !== $signup->save()) {
+                return $this->render('confirmation', [
+                ]);
+            }
         }
 
         return $this->render('index', [
---
+++ models/SignupForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use humanized\usermanagement\common\models\UserCrud;
+
+/**
+ * Signup form
+ */
+class SignupForm extends Model
+{
+
+    public $email;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['email', 'filter', 'filter' => 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['email', 'unique', 'targetClass' => '\app\models\User', 'message' => 'This email address has already been taken.'],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function save()
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+
+        $user = UserCrud::create(['email' => $this->email]);
+     
+        return $user;
+    }
+
+
+}
